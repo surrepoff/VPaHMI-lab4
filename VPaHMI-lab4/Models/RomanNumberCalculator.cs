efb60e726626b78495a712ffaf2c43119953e196
surrepoff <alexander.bessonov548@gmail.com>
2022-03-10 20:15:20 +0700

    Update

--- VPaHMI-lab4/Models/RomanNumberCalculator.cs
+++ VPaHMI-lab4/Models/RomanNumberCalculator.cs
@@ -10,40 +10,22 @@ namespace VPaHMI_lab4.Models
 {
     public static class RomanNumberCalculator
     {
-
-        private static ushort CheckOperator(string input)
-        {
-            if (input.Count(f => f == '+') + input.Count(f => f == '-') + input.Count(f => f == '*') + input.Count(f => f == '/') == 1)
-            {
-                if (input.Count(f => f == '+') == 1) return 1;
-                if (input.Count(f => f == '-') == 1) return 2;
-                if (input.Count(f => f == '*') == 1) return 3;
-                if (input.Count(f => f == '/') == 1) return 4;
-            }
-            return 0;
-        }
-
         public static RomanNumberExtend Calculate(string input)
         {
-            ushort _operator = CheckOperator(input);
-            if (_operator == 0) throw new RomanNumberException("ERROR: Invalid input");
-            string[] number = input.Split('+', '-', '*', '/');
-            if (!RomanNumberExtend.CheckString(number[0])) throw new RomanNumberException("ERROR: Invalid input");
-            if (!RomanNumberExtend.CheckString(number[2])) throw new RomanNumberException("ERROR: Invalid input");
+            var splittedInputByOperands = Regex.Split(input, @"([*()\^\/]|(?<!E)[\+\-])");
 
-            switch (_operator)
+            string output = "";
+            foreach (var element in splittedInputByOperands)
             {
-                case 1: return (RomanNumberExtend)(new RomanNumberExtend(RomanNumberExtend.ToUshort(number[0])) + new RomanNumberExtend(RomanNumberExtend.ToUshort(number[1])));
-                case 2: return (RomanNumberExtend)(new RomanNumberExtend(RomanNumberExtend.ToUshort(number[0])) - new RomanNumberExtend(RomanNumberExtend.ToUshort(number[1])));
-                case 3: return (RomanNumberExtend)(new RomanNumberExtend(RomanNumberExtend.ToUshort(number[0])) * new RomanNumberExtend(RomanNumberExtend.ToUshort(number[1])));
-                case 4: return (RomanNumberExtend)(new RomanNumberExtend(RomanNumberExtend.ToUshort(number[0])) / new RomanNumberExtend(RomanNumberExtend.ToUshort(number[1])));
-                default:
-                    break;
+                if (element != "+" && element != "-" && element != "*" && element != "/")
+                {
+                    if (!RomanNumberExtend.CheckString(element)) throw new RomanNumberException("Invalid input");
+                    output += (new RomanNumberExtend(element)).UshortValue().ToString();
+                }
+                else output += element;
             }
-            return new RomanNumberExtend(0);
+            DataTable dt = new();
+            return new RomanNumberExtend(Convert.ToUInt16(dt.Compute(output, "")));
         }
-
-
-
     }
 }
